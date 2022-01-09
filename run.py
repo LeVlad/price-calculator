fb1970cd1acb30d72fdc8cfcf938e323e7ac352a
LeVlad <leca.vlad@gmail.com>
2022-01-09 23:29:24 +0000

    Add input request for values

--- run.py
+++ run.py
@@ -15,4 +15,35 @@ SHEET = GSPREAD_CLIENT.open("price-calculator")
 sockets = SHEET.worksheet("sockets")
 
 data = sockets.get_all_values()
-print(data)
+
+sockets_value = 0
+lights_value = 0
+switches_value = 0
+
+def get_values():
+    """
+    Get the values from the user
+    """
+
+    
+print("Welcome to Materials Calculator\n")
+print("""    -------1. Sockets --------\n
+     -------2. Lights -------\n
+    -------3. Switches -------\n""")
+
+answer = ""
+answer = input("Please select one of the options above:")
+while True:
+    if answer == "1":
+        print("You have selected sockets.Fetching price...")
+        break
+    elif answer == "2":
+        print("You have selected lights.Fetching price...")
+        break
+    elif answer == "3":
+        print("You have selected switches.Fetching price...")
+        break
+    else:
+        input("Please select one of the options above:")
+
+    
