ab6f1c39d146e5b63cc7036742b6cadebe99c8ef
LeVlad <leca.vlad@gmail.com>
2022-01-11 23:36:41 +0000

    Add initial function to print all values and prices

--- run.py
+++ run.py
@@ -11,39 +11,58 @@ CREDS = Credentials.from_service_account_file("creds.json")
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("price-calculator")
+SOCKETS = SHEET.worksheet('sockets')
+LIGHTS = SHEET.worksheet('lights')
+SWITCHES = SHEET.worksheet('switches')
 
-sockets = SHEET.worksheet("sockets")
 
-data = sockets.get_all_values()
 
-sockets_value = 0
-lights_value = 0
-switches_value = 0
-
-def get_values():
+def view_stock():
     """
-    Get the values from the user
+    A function that prints out the current stock of materials with prices
     """
 
-    
-print("Welcome to Materials Calculator\n")
-print("""    -------1. Sockets --------\n
-     -------2. Lights -------\n
-    -------3. Switches -------\n""")
+    print("Please select an option from the menu")
 
-answer = ""
-answer = input("Please select one of the options above:")
-while True:
-    if answer == "1":
-        print("You have selected sockets.Fetching price...")
-        break
-    elif answer == "2":
-        print("You have selected lights.Fetching price...")
-        break
-    elif answer == "3":
-        print("You have selected switches.Fetching price...")
-        break
+    stock_str = input('Enter your choice here:')
+    sockets_stock = stock_str.split(',')
+   
+    def main():
+        """
+        Start menu that the user can choose between 4 different tasks.
+        """
+    print("""   --------MENU--------
+                1.View Stock List w/Prices\n\
+                2.Add Sales Data\n\
+                3.Add Stock Delivery\n\
+                4.Do Stock Take\n\
+                    """)
+
+    def view_stock():
+      """
+    Function to get all values from google sheet
+    and show them as a list for each type.
+     """
+
+    view_stocks = SOCKETS.get_all_values()
+    if view_stock:
+        for socket in view_stocks:
+            printing_all_values(socket)
     else:
-        input("Please select one of the options above:")
+        print("Stock is empty!")
+    back_to_menu()
 
-    
+view_stock()    
+
+def printing_all_values(sockets):
+    """
+    Function that takes all the existing values from worksheet
+    and make it a loop with the materials type and prices
+    """
+    one_value = []
+    for type, price in sockets.items():
+        print(f'{type}: {price}')
+    print("-----------------------------------")
+    return one_value
+
+
