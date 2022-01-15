8875e85f188d663f78aa7847b95331c95498d070
LeVlad <leca.vlad@gmail.com>
2022-01-15 18:45:56 +0000

    Add new start function

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from tabulate import tabulate
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -18,73 +19,45 @@ LIGHTS = SHEET.worksheet("lights")
 SWITCHES = SHEET.worksheet("switches")
 SALES = SHEET.worksheet("sales")
 
-sockets_data = SOCKETS.get_all_values()
-lights_data = LIGHTS.get_all_values()
-switches_data = SWITCHES.get_all_values()
-sales_data = SALES.get_all_values()
-
-enter_input = ("Please select from the options above:\n")
-answer = ""
-
-
-def get_sales_data():
+def start():
     """
-    Get sales figures input from the user.
+    Start menu that the user can choose between 6 different tasks.
     """
-    print("Please enter up to date sales figures.")
-    print("Data should be 3 numbers, separated by commas.")
-    print("Example: 10,20,30\n")
-    print(input("Please insert sales values:"))
-
-#get_sales_data()
-
-#def update_sales():
-    """ Function to update sales sheet within google spreadsheet
-    """
-    get_sales_data()
-    print("Updating worksheet...\n")
-    sales_worksheet = SHEET.worksheet(sales)
-    sales_worksheet.append_row(data)
-    print("Sales worksheet updated successfully!")
-
-    #update_sales()
-
-def main_menu():
-    """
-    Function that brings up the main menu.
-    """
-while True:
-    print("""---MENU---
-        1.View Materials and Price
-        2.Add Delivery Data
-        3.Add Sales Data
-        4.Stock Take
-        """)
-    answer = input(enter_input)
-    if answer == "1":
-        print(
-            """ 
-        1.Sockets
-        2.Lights
-        3.Switches
-
-            """)
-        input("Please choose one option:\n")
-        if answer == "1":
-            sockets_materials = [item[2:] for item in sockets_data]
-            print(sockets_materials)
-            sockets_materials_view = [item[1:] for item in sockets_materials]
-            print(sockets_materials_view)
-           
-        break
-    elif answer == "2":
-            list_of_lists = SOCKETS.get_all_records()
-    elif answer == "3":
-            stock_data = ([item[4:] for item in sockets_data])
-            input_data = []          
-            input_data = print("Please input what stock has been delivered")
-            print(f'{stock_data} - {input_data}')
+    print("""
+                --------MENU--------
+                1. View sockets stock and price\n\
+                2. View lights stock and price\n\
+                3. View switches stock and price\n\
+                4. Update stock\n\
+                5. Get quote\n\
+                6. Exit\n
+                    """)
+    while True:
+        answer = input("Choose an option: \n")
+        if answer == '1':
+            print("Taking you to View sockets stock and price...\n")
+            sockets_stock_price = SOCKETS.get_all_records()
             break
-
-main_menu()
+        elif answer == '2':
+            print("Taking you to View lights stock and price......\n")
+            show_all_contacts()
+            break
+        elif answer == '3':
+            print("Taking you to View switches stock and price......\n")
+            search_contact()
+            break
+        elif answer == '4':
+            print("Taking you to Update stock...\n")
+            search_contact()
+            break
+        elif answer == '5':
+            print("Taking you to Get quote...\n")
+            validate_reset()
+            break
+        elif answer == '6':
+            print("Exiting...")
+            exit_programme()
+            break
+        else:
+            print("Not a valid input please enter a number 1-6")
 
