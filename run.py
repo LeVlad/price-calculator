42c10c4b4aadefa8f4e6353b8fb351e48765f5b2
LeVlad <leca.vlad@gmail.com>
2022-01-16 03:12:26 +0000

    Add structure and functionality to main menu function

--- run.py
+++ run.py
@@ -19,6 +19,65 @@ LIGHTS = SHEET.worksheet("lights")
 SWITCHES = SHEET.worksheet("switches")
 SALES = SHEET.worksheet("sales")
 
+
+
+def get_sales_data():
+    """
+    Get sales figures input from the user.
+    """
+    while True:
+        print("Please enter sales data from the last market.")
+        print("Data should be three numbers, separated by commas.")
+        print("Example: 10,20,30\n")
+
+        data_str = input("Enter your data here: ")
+        sales_data = data_str.split(",")
+        validate_data(sales_data)
+        
+        if validate_data(sales_data):
+            print("Data is valid!")
+        break
+    
+
+def update_worksheet(data, worksheet):
+    """
+    Receives a list of integers to be inserted into a worksheet
+    Update the relevant worksheet with the data provided
+    """
+    print(f"Updating {worksheet} worksheet...\n")
+    worksheet_to_update = SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(data)
+    print(f"{worksheet} worksheet updated successfully\n")
+
+        
+def validate_data(values):
+    """
+    Inside the try, converts all string values into integers.
+    Raises ValueError if strings cannot be converted into int,
+    or if there aren't exactly 3 values.
+    """
+    try:
+        [int(value) for value in values]
+        if len(values) != 3:
+            raise ValueError(
+                f"Exactly 3 values required, you provided {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
+        return False
+    return True
+get_sales_data()
+
+def exit_programme():
+    """
+    Shutting down program when user chose the exit task in menu
+    """
+    print("|*******************************************************|")
+    print("|***************Thank you for using this****************|")
+    print("|***********************APP!****************************|")
+    print("|************************GOODBYE************************|")
+    print("|*******************************************************|")
+
 def start():
     """
     Start menu that the user can choose between 6 different tasks.
@@ -28,7 +87,7 @@ def start():
                 1. View sockets stock and price\n\
                 2. View lights stock and price\n\
                 3. View switches stock and price\n\
-                4. Update stock\n\
+                4. Update sales\n\
                 5. Get quote\n\
                 6. Exit\n
                     """)
@@ -36,25 +95,35 @@ def start():
         answer = input("Choose an option: \n")
         if answer == '1':
             print("Taking you to View sockets stock and price...\n")
+            print("ALL PRICES ARE FOR SINGLE PARTS\n")
             socket_type = SOCKETS.col_values(1)
             socket_stock = SOCKETS.col_values(5)
-            print(f'{socket_type}:{socket_stock}')
+            socket_price = SOCKETS.col_values(2)
+            socket_size = SOCKETS.col_values(3)
+            print(f'{socket_type}\n:{socket_size}\n:{socket_price}\n:{socket_stock}')
             break
         elif answer == '2':
             print("Taking you to View lights stock and price......\n")
+            print("ALL PRICES ARE FOR SINGLE PARTS\n")
             lights_type = LIGHTS.col_values(1)
+            lights_wattage = LIGHTS.col_values(2)
+            lights_price = LIGHTS.col_values(3)
             lights_stock = LIGHTS.col_values(4)
-            print(f'{lights_type}:{lights_stock}')
+            print(f'{lights_type}\n:{lights_wattage}\n:{lights_price}\n:{lights_stock}')
             break
         elif answer == '3':
             print("Taking you to View switches stock and price......\n")
+            print("ALL PRICES ARE FOR SINGLE PARTS\n")
             switches_type = SWITCHES.col_values(1)
+            switches_price = SWITCHES.col_values(3)
             switches_stock = SWITCHES.col_values(4)
-            print(f'{switches_type}:{switches_stock}')
+            print(f'{switches_type}\n:{switches_price}\n:{switches_stock}')
             break
         elif answer == '4':
-            print("Taking you to Update stock...\n")
-           
+            print("Taking you to Update sales...\n")
+            get_sales_data()
+            validate_data()
+            update_worksheet()
             break
         elif answer == '5':
             print("Taking you to Get quote...\n")
@@ -66,15 +135,20 @@ def start():
             break
         else:
             print("Not a valid input please enter a number 1-6")
-            
-start()
 
-def exit_programme():
+def main():
     """
-    Shutting down program when user chose the exit task in menu
+    Function that runs all functions
     """
-    print("-------------------------------------------------------")
-    print("---------------Thank you for using this----------------")
-    print("------------------------APP!---------------------------")
-    print("------------------------GOODBYE------------------------")
-    print("-------------------------------------------------------")
+    start()
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_worksheet(sales_data, "sales")
+    
+    
+    
+main()
+
+
+
+
